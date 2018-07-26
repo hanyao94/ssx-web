7cd0b9b29fc01ddb62f75c50af88dca8f4e319c0
hanyao94 <hanyao.huang@foxmail.com>
2018-07-26 20:08:14 +0800

    1.调整request请求封装的方法，对request进行拦截加入token，对response 进行拦截处理401的错误->跳转到登录页面 2.修改后端服务请求地址dev.env文件

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,15 +4,23 @@ import store from '../store'
 import { getToken } from '@/utils/token'
 
 // 创建axios实例
+// https://www.kancloud.cn/yunye/axios/234845
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000 // 请求超时时间
+  timeout: 5000, // 请求超时时间
+  // 所有请求都以Json形式传送
+  // 会有预检请求，服务端需要正常通过OPTIONS请求
+  // http://www.ruanyifeng.com/blog/2016/04/cors
+  headers: {
+    'Content-type': 'application/json;charset=UTF-8'
+  }
 })
 
 // request拦截器
 service.interceptors.request.use(config => {
   if (store.getters.token) {
-    config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    // 让每个请求携带自定义token 请根据实际情况自行修改
+    config.headers['Authorization'] = getToken()
   }
   return config
 }, error => {
@@ -21,44 +29,39 @@ service.interceptors.request.use(config => {
   Promise.reject(error)
 })
 
-// respone拦截器
+// response拦截器
 service.interceptors.response.use(
   response => {
-  /**
-  * code为非20000是抛错 可结合自己业务进行修改
-  */
-    const res = response.data
-    if (res.code !== 20000) {
+    if (response.data.code === 200) {
+      return response.data
+    } else {
       Message({
-        message: res.message,
+        message: response.data.msg,
         type: 'error',
         duration: 5 * 1000
       })
-
-      // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('FedLogOut').then(() => {
-            location.reload()// 为了重新实例化vue-router对象 避免bug
-          })
-        })
-      }
       return Promise.reject('error')
-    } else {
-      return response.data
     }
   },
   error => {
-    console.log('err' + error)// for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
+    // 401:需要认证
+    if (error.response.code === 401) {
+      MessageBox.confirm('Need login', 'logout', {
+        confirmButtonText: 'login',
+        cancelButtonText: 'cancel',
+        type: 'warning'
+      }).then(() => {
+        store.dispatch('FedLogout').then(() => {
+          location.reload()// 为了重新实例化vue-router对象 避免bug
+        })
+      })
+    } else {
+      Message({
+        message: error.response.data.msg,
+        type: 'error',
+        duration: 5 * 1000
+      })
+    }
     return Promise.reject(error)
   }
 )
